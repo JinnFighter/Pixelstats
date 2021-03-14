a9cc7c7557640d4f30d1020edce49e65b99656ba
Jinn <chernayamolnia@yandex.ru>
2021-03-14 13:22:27 +0300

    add Stats model

---
+++ Models/Stats.cs
@@ -0,0 +1,11 @@
+﻿namespace Pixelstats.Models
+{
+    public class Stats
+    {
+        public float Time { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int WrongAnswers { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
