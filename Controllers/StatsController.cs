2bd1240002c23ead0912403cdecd973326298600
Jinn <chernayamolnia@yandex.ru>
2021-03-14 15:10:25 +0300

    add StatsController

---
+++ Controllers/StatsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+namespace Pixelstats.Controllers
+{
+    public class StatsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Statistics";
+            return View();
+        }
+    }
+}
