be90c275b46eca1f12c2eeb26901d8279ce7c3b6
Jinn <chernayamolnia@yandex.ru>
2021-03-14 15:54:51 +0300

    add StatsListViewModel

---
+++ ViewModels/StatsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Pixelstats.Models;
+using System.Collections.Generic;
+
+namespace Pixelstats.ViewModels
+{
+    public class StatsListViewModel
+    {
+        public IEnumerable<StatData> Stats { get; set; }
+    }
+}
