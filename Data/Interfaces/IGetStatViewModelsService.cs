7336e9fa98b227d0e1f8445116e5cb1764d11297
Jinn <chernayamolnia@yandex.ru>
2021-03-27 15:12:21 +0300

    add IGetStatViewModelsService interface

---
+++ Data/Interfaces/IGetStatViewModelsService.cs
@@ -0,0 +1,11 @@
+﻿using Pixelstats.Models;
+using Pixelstats.ViewModels;
+using System.Collections.Generic;
+
+namespace Pixelstats.Data.Interfaces
+{
+    interface IGetStatViewModelsService
+    {
+        IEnumerable<StatDataViewModel> GetViewModels(IEnumerable<StatData> datas);
+    }
+}
