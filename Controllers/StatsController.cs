d5a933d41f519143c7344c4e7934b509d970c5c4
Jinn <chernayamolnia@yandex.ru>
2021-03-27 16:06:35 +0300

    add temporary StatsController.Index logic

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -1,44 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Pixelstats.Data.Interfaces;
-using Pixelstats.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Pixelstats.Controllers
 {
     public class StatsController : Controller
     {
-        private readonly IGetStats _stats;
+        private readonly IGetUsers _users;
 
-        public StatsController(IGetStats stats)
+        public StatsController(IGetUsers users)
         {
-            _stats = stats;
+            _users = users;
         }
 
         public IActionResult Index()
         {
-            ViewBag.Title = "Statistics";
-
-            var stats = new List<StatDataViewModel>();
-
-            var allStats = _stats.GetStats.ToList();
-            if (allStats.Count == 0)
-                throw new Exception("STATS EMPTY");
-            foreach(var stat in _stats.GetStats)
-            {
-                stats.Add(new StatDataViewModel
-                {
-                    GameModeName = stat.GameMode.Name,
-                    Time = string.Format("{0:00} m:{1:00} s", (int)(stat.Time / 60f) % 60,
-                    (int)(stat.Time % 60)),
-                    CorrectAnswers = (uint)stat.CorrectAnswers,
-                    WrongAnswers = (uint)stat.WrongAnswers,
-                    SuccessRate = stat.WrongAnswers == 0 ? 0 : (int)((double)stat.CorrectAnswers / (double)(stat.CorrectAnswers + stat.WrongAnswers) * 100)
-                });
-            }
-
-            return PartialView("_StatsListPartial", stats);
+            ViewBag.Title = _users.GetUsers().First().UserName;
+            return View();
         }
     }
 }
