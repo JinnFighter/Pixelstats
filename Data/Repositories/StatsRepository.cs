95b45d4fffc0b9f1d1245bf16a3f41c2762363ca
Jinn <chernayamolnia@yandex.ru>
2021-04-25 00:01:17 +0300

    StatsRepository now implements IStatUpdater interface

--- Data/Repositories/StatsRepository.cs
+++ Data/Repositories/StatsRepository.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace Pixelstats.Data.Repositories
 {
-    public class StatsRepository : IGetStats
+    public class StatsRepository : IGetStats, IStatUpdater
     {
         private readonly AppDbContext _context;
 
@@ -15,5 +15,7 @@ namespace Pixelstats.Data.Repositories
         }
 
         public IEnumerable<StatData> GetStats => _context.StatDatas.Include(statData => statData.GameMode).Include(statData => statData.User);
+
+        public void AddStats(StatData statData) => _context.StatDatas.Add(statData);
     }
 }
