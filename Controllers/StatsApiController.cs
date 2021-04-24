51367b3727a9c8ba7ba4dcd62d33f4de35277a20
Jinn <chernayamolnia@yandex.ru>
2021-04-24 15:45:18 +0300

    StatsApiController now returns ApiData on GetRequest

--- Controllers/StatsApiController.cs
+++ Controllers/StatsApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pixelstats.Models;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,16 +12,34 @@ namespace Pixelstats.Controllers
     {
         // GET: api/<StatsApiController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<ApiData> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new List<ApiData>
+            {
+                new ApiData
+                {
+                    PlayerName = "test",
+                    GameModeName = "testMode",
+                    Time = 30f,
+                    CorrectAnswers = 1,
+                    WrongAnswers = 2
+                }
+            };
         }
 
         // GET api/<StatsApiController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ApiData Get(int id)
         {
-            return "value";
+            return
+                new ApiData
+                {
+                    PlayerName = "test",
+                    GameModeName = "testMode",
+                    Time = 30f,
+                    CorrectAnswers = 1,
+                    WrongAnswers = 2
+            };
         }
 
         // POST api/<StatsApiController>
