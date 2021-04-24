329f27a80e25c912d6a16c921a8b94ab338dec21
Jinn <chernayamolnia@yandex.ru>
2021-04-24 15:33:53 +0300

    add ApiData model

---
+++ Models/ApiData.cs
@@ -0,0 +1,11 @@
+﻿namespace Pixelstats.Models
+{
+    public class ApiData
+    {
+        public string PlayerName { get; set; }
+        public string GameModeName { get; set; }
+        public float Time { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int WrongAnswers { get; set; }
+    }
+}
