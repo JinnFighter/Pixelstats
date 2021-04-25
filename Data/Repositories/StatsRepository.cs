a08e6668e2553eb5ab744bb80253222054c09328
Jinn <chernayamolnia@yandex.ru>
2021-04-25 11:29:55 +0300

    fix StatsRepository not saving changes to database

--- Data/Repositories/StatsRepository.cs
+++ Data/Repositories/StatsRepository.cs
@@ -16,6 +16,10 @@ namespace Pixelstats.Data.Repositories
 
         public IEnumerable<StatData> GetStats => _context.StatDatas.Include(statData => statData.GameMode).Include(statData => statData.User);
 
-        public void AddStats(StatData statData) => _context.StatDatas.Add(statData);
+        public void AddStats(StatData statData) 
+        { 
+            _context.StatDatas.Add(statData);
+            _context.SaveChanges();
+        }
     }
 }
