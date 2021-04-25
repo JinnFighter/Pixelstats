750231e7467ce46db0d6f60ded087f6ce82d0582
Jinn <chernayamolnia@yandex.ru>
2021-04-25 11:22:24 +0300

    add GameModesRepository

---
+++ Data/Repositories/GameModesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Pixelstats.Data.Interfaces;
+using Pixelstats.Models;
+using System.Collections.Generic;
+
+namespace Pixelstats.Data.Repositories
+{
+    public class GameModesRepository : IGetGameModes
+    {
+        private readonly AppDbContext _context;
+
+        public GameModesRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<GameMode> GetGameModes() => _context.GameModes;
+    }
+}
